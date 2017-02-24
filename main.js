f6c6df3178b63df9dae1807ed54269d3008ba08e
AdamConverse <adam.converse@gmail.com>
2017-02-23 18:09:35 -0600

    updates

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-electron.crashReporter.start();
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
 
-var mainWindow = null;
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
 
-app.on('window-all-closed', function() {
-  //if (process.platform != 'darwin') {
-    app.quit();
-  //}
-});
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
 
-app.on('ready', function() {
-  // call python?
-  var subpy = require('child_process').spawn('python', ['./hello.py']);
-  //var subpy = require('child_process').spawn('./dist/hello.exe');
-  var rq = require('request-promise');
-  var mainAddr = 'http://localhost:5000';
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-  var openWindow = function(){
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    // mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.loadURL('http://localhost:5000');
-    mainWindow.webContents.openDevTools();
-    mainWindow.on('closed', function() {
-      mainWindow = null;
-      subpy.kill('SIGINT');
-    });
-  };
+  // Open the DevTools.
+  win.webContents.openDevTools()
 
-  var startUp = function(){
-    rq(mainAddr)
-      .then(function(htmlString){
-        console.log('server started!');
-        openWindow();
-      })
-      .catch(function(err){
-        //console.log('waiting for the server start...');
-        startUp();
-      });
-  };
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
 
-  // fire!
-  startUp();
-});
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
